a76b817ef5639b27904b86a1bfd8e8e42a5317a3
Owlthentic <colin.huwiler@stud.hslu.ch>
2024-11-25 11:44:57 +0100

    modified:   connect4/display_console.py 	modified:   connect4/player_console.py

--- connect4/display_console.py
+++ connect4/display_console.py
@@ -1,37 +1,100 @@
 from ansi import Ansi
 from game_token import GameToken
 from display_base import DisplayBase
-
+import os
 
 class DisplayConsole(DisplayBase):
-    pass
+    def __init__(self):
+        self.gen_grid()
+
+    def _clear_console(self):
+        os.system('clear')
+        os.system('cls')    
+
+    @ staticmethod
+    def gen_row(left:str, middle:str, right:str, spacer:str):
+        row = ""
+        row += left + spacer
+        for cell in range(7-2):
+            row += middle + spacer
+        row += middle + spacer + right
+        return row
+    
+        
+    def gen_grid(self):
+        horizontal_spacer = "───"
+        void_spacer = "   "
+
+        top_row = self.gen_row("┌", "┬", "┐", horizontal_spacer)
+        middle_row = self.gen_row("├", "┼", "┤", horizontal_spacer)
+        bottom_row = self.gen_row("└", "┴", "┘", horizontal_spacer)
+        spacer = self.gen_row("│", "│", "│", void_spacer)
+
+        self.grid = ""
+        self.grid += top_row + "\n"
+        self.grid += spacer + "\n"
+
+        for row_1 in range(6-1):
+            self.grid += middle_row + "\n"
+            self.grid += spacer + "\n"
+        self.grid += bottom_row
 
 
-"""
-┌
-┐
-└
-┘
-├
-┤
-┼
-─
-│
-┬
-┴
-█ 
+    def draw_grid(self):
+        self._clear_console
+        Ansi.clear_screen()
+        Ansi.reset()
 
-https://de.wikipedia.org/wiki/Unicodeblock_Rahmenzeichnung
-"""
+        Ansi.gotoXY(0, 3)
+        print(self.grid)
+
+        Ansi.gotoXY(0, 16)
+        
+    def draw_token(self, x_pos, y_pos):
+        Ansi.gotoXY(self.x_translation(x_pos), 
+                    self.y_translation(y_pos))
+        print("●")
+        Ansi.gotoXY(0, 16)
+        
+    @ staticmethod
+    def y_translation(y_pos):
+        y = 2 + 2 + y_pos * 2
+        return y
+    @ staticmethod
+    def x_translation(x_pos):
+        x = 3 + x_pos * 4
+        return x
+    
+    def draw_input(self, column):
+        self.clear_input()
+
+        y = 2
+        x = self.x_translation(column)
+
+        Ansi.gotoXY(x, y)
+        print("●")
+        
+        Ansi.gotoXY(0, 16)
+
+    @ staticmethod
+    def clear_input():
+        y = 2
+        empty_line = " " * (7 * 4 + 1)
+
+        Ansi.gotoXY(0, y)
+        print(empty_line, end="")
+
+        Ansi.gotoXY(0, 16)
+
+
+    
+
 
 
 if __name__ == '__main__':
-    Ansi.clear_screen()
-    Ansi.reset()
     fc = DisplayConsole()
     fc.draw_grid()
-    fc.draw_token(0, 0, GameToken.RED)
-    fc.draw_token(5, 2, GameToken.YELLOW)
-    Ansi.gotoXY(1, 20)
-    print(type(GameToken.RED))
-    print(GameToken.RED)
+    fc.draw_token(0, 0)
+    fc.draw_token(5, 2)
+    fc.draw_input(1)
+    fc.draw_input(2)
